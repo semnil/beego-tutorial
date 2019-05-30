14576ff5014390e34b6df9454481382ce6352bcb
hassmo2019 <mei.ogido@cydas.com>
2019-05-30 17:39:07 +0900

    aaa

--- quickstart/controllers/default.go
+++ quickstart/controllers/default.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"context"
+	"github.com/aws/aws-xray-sdk-go/xray"
+	_ "github.com/aws/aws-xray-sdk-go/plugins/beanstalk"
 )
 
 type MainController struct {
@@ -9,7 +12,9 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	_, seg := xray.BeginSegment(context.Background(), "/")
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+	seg.Close(nil)
 }
